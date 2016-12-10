67cf02fba6298554393f3ae4b2eff900e0eb13c0
Cyprian <cyprian.manthey@cs.tum.edu>
2016-12-10 05:16:56 +0100

    PlayerScript
    seasons and script

---
+++ Guenther/Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+    public enum seasons { spring, summer, autumn, winter };
+    public MoveSettings moveSettings;
+    public InputSettings inputSettings;
+    public Transform spawnPoint;
+    public static seasons actualSeason;
+    private float sidewaysInput;
+    private float jumpInput;
+    private GameObject player;
+    private Vector3 velocity;
+
+    private void Awake() {
+        sidewaysInput = jumpInput = 0;
+        player = this.gameObject;
+        actualSeason = seasons.spring;
+    }
+
+    void Update() {
+        GetPlayerInput();
+        Run();
+        Jump();
+    }
+
+    void GetPlayerInput() {
+        sidewaysInput = Input.GetAxis(inputSettings.PLAYER_SIDEWAYS_AXIS);
+        jumpInput = Input.GetAxisRaw(inputSettings.PLAYER_JUMP_AXIS);
+    }
+
+    void Spawn() {
+        transform.position = spawnPoint.position;
+    }
+    void Run() {
+        velocity = new Vector3(sidewaysInput * moveSettings.RunVelocity, player.GetComponent<Rigidbody2D>().velocity.y);
+        player.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(velocity);
+    }
+
+    void Jump() {
+        if (jumpInput != 0 && playerGrounded()) {
+            player.GetComponent<Rigidbody2D>().velocity = new Vector3(player.GetComponent<Rigidbody2D>().velocity.x, moveSettings.JumpVelocity, velocity.z);
+        }
+    }
+
+    bool playerGrounded() {
+        RaycastHit2D hit = Physics2D.Raycast(player.transform.position, Vector3.down, moveSettings.DistanceToGround, moveSettings.Ground);
+        Debug.DrawRay(player.transform.position, Vector3.down, Color.red, 1f);
+        if (hit) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    [System.Serializable]
+    public class MoveSettings {
+        public float RunVelocity = 0.8f;
+        public float JumpVelocity = 100f;
+        public float DistanceToGround = 4f;
+        public LayerMask Ground;
+    }
+
+    [System.Serializable]
+    public class InputSettings {
+        public string PLAYER_SIDEWAYS_AXIS = "Horizontal";
+        public string PLAYER_JUMP_AXIS = "Jump";
+    }
+}
