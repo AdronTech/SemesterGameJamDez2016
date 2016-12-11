aaee1f8d12e7e4065e407b23e52e37c5c1540baa
LEM-II\Ilja <>
2016-12-11 11:07:34 +0100

    more sounds tile spwn and victory

--- Guenther/Assets/Scripts/TileBehaviour_Victory.cs
+++ Guenther/Assets/Scripts/TileBehaviour_Victory.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TileBehaviour_Victory : MonoBehaviour {
 
+    public AudioClip victorySound;
+
 	void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(4); 
+            StartCoroutine(Victory());
         }
+    }
+
+    IEnumerator Victory()
+    {
+        AudioSource src = GetComponent<AudioSource>();
+        if (src == null)
+        {
+            src = gameObject.AddComponent<AudioSource>();
+        }
+        src.clip = victorySound;
+        src.loop = false;
+        src.Play();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(4);
     }
 }
