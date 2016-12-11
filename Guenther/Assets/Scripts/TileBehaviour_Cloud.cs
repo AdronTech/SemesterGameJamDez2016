92261e0282bea9b34eeddac0de7c07758fdb0f4b
LEM-II\Ilja <>
2016-12-11 01:42:46 +0100

    comiiiiiit

--- Guenther/Assets/Scripts/TileBehaviour_Cloud.cs
+++ Guenther/Assets/Scripts/TileBehaviour_Cloud.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TileBehaviour_Cloud : MonoBehaviour {
-
+    public ParticleSystem ps_rain;
     private int lastSeason = 0;
     private enum State { None, Raining };
     private State curState = State.None;
@@ -32,9 +32,13 @@ public class TileBehaviour_Cloud : MonoBehaviour {
     private void changeState() {
         switch (curState) {
             case State.Raining:
+                ps_rain.Play();
+                GetComponent<SpriteRenderer>().enabled = true;
                 gameObject.GetComponent<BoxCollider2D>().enabled = true;
                 break;
             case State.None:
+                GetComponent<SpriteRenderer>().enabled = false;
+                ps_rain.Stop();
                 gameObject.GetComponent<BoxCollider2D>().enabled = false;
                 break;
             default:
