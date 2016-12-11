c88a51c401fae66227e9015508b40ff1b6c38686
Cyprian <cyprian.manthey@cs.tum.edu>
2016-12-11 07:03:20 +0100

    added capsule collider, and apples

--- Guenther/Assets/Scripts/Player.cs
+++ Guenther/Assets/Scripts/Player.cs
@@ -123,7 +123,7 @@ public class Player : MonoBehaviour
         if (other.gameObject.tag == "apple")
         {
             appleCount++;
-            Destroy(other);
+            Destroy(other.gameObject);
         }
 
     }
